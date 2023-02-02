540fff9f4378289edd4c24b7e7e5a98975b3c80c
sfincks <sfincks666@gmail.com>
2023-02-02 17:40:26 +0500

    zadacha_01

--- Zadach_01/Program.cs
+++ Zadach_01/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8 
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number <= 99 || number > 999)&&(number >= -99 || number < -999))
+{
+    System.Console.WriteLine("Вы ввели не трехзначное число");
+}
+else
+{
+    System.Console.WriteLine(Math.Abs(number % 100) / 10);
+}
