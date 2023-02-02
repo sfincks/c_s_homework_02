291539dfd40b0b45b02e1503e64179787a563466
sfincks <sfincks666@gmail.com>
2023-02-02 18:57:57 +0500

    the end of the homework

--- Zadach_02/Program.cs
+++ Zadach_02/Program.cs
@@ -1,7 +1,27 @@
-﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+﻿
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 // что третьей цифры нет.
 
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
+System.Console.WriteLine("Введите число");
+start:
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = -1;
+if (num < 100)
+{
+    System.Console.WriteLine("Введите больше число 100");
+    goto start;
+}
+else if (num >= 100)
+    {
+        while (num > 999)
+        {
+            num = num / 10;
+        }
+        result = num % 10;
+    }
+    System.Console.WriteLine(result); 
